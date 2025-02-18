8ded506035503402dddafe716d9553a5150041fd
parasohri <150348022+parasohri@users.noreply.github.com>
2025-02-18 18:57:14 +0530

    remove timeout setting from axios instance configuration

--- frontend/axios/axios.js
+++ frontend/axios/axios.js
@@ -3,7 +3,7 @@
 // Create an instance of axios with custom configuration
 export const apiClient = axios.create({
     baseURL: 'https://task-manger-kappa.vercel.app/', // Replace with your API base URL
-    timeout: 1000,
+    // timeout: 1000,
     headers: { 'Content-Type': 'application/json' }
 });
 
